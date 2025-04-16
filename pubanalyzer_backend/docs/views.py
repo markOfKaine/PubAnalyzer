20b3282ba0bc04a0d256e13c6e54334655fbda96
Edmond Abraham <3dm0nd6@gmail.com>
2025-04-16 16:08:41 -0500

    Built API endpoints for the USER model with username, name, and email validation

--- pubanalyzer_backend/docs/views.py
+++ pubanalyzer_backend/docs/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework import serializers
+from .serializers import UserSerializer
 
-def test_api(request):
-    return JsonResponse({"message": "Hello from Django!"})
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDeleteView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
