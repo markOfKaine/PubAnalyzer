e859014d0da591014637e2cfff76323ec56cddfe
Tim West <timr200019@yahoo.com>
2025-04-16 18:26:12 -0500

    Refined Navigation Bar
    A: Implemented ability to use Shadcn UI components
    A: Added Avatar dropdown icon and Customizable Theme Colors
    A: Added Initial PDF Upload Box
    C: Refactored navigation bar with Shadcn components
    C: Changed navigation bar layout to be more mobile friendly

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -5,14 +5,14 @@ import { Search } from "lucide-react";
 function Home() {
   return (
     <div className="max-w-2xl mx-auto mt-4 text-center">
-      <h1 className="text-3xl font-bold mb-6">Welcome to PubAnalyzer</h1>
-      <p className="text-lg mb-8">
+      <h1 className="text-3xl font-bold mb-6 text-foreground">Welcome to PubAnalyzer</h1>
+      <p className="text-lg mb-8 text-secondary-foreground">
         An easy way to view, analyze, and mark up PubMed Central articles!
       </p>
       <Link href="/analyze">
         <button
           className="inline-flex items-center space-x-2 px-16 py-3 rounded-lg
-          bg-primary hover:bg-primary-dark text-secondary font-semibold"
+          bg-primary hover:bg-primary/50 text-background font-semibold"
         >
           <Search size={20} />
           <span>Analyze</span>
