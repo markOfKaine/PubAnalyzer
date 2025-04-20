a33610e3395812af5a2615ae9b289089dc3ea6a1
Tim West <timr200019@yahoo.com>
2025-04-20 00:10:50 -0500

    Added Improved Viewer Page
    C: Viewer now uses a sticky sidebar instead of a header based nav bar
    C: Annotation bar can now be collpased

---
+++ frontend/app/(sidebarOnly)/viewer/page.tsx
@@ -0,0 +1,277 @@
+"use client";
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import AnnotationBar from "@/components/AnnotationBar";
+import CustomTip from "@/components/CustomTip";
+import AiTip from "@/components/AiTip";
+import AppSidebar from "@/components/app-side-bar";
+
+import {
+  SidebarInset,
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarInput,
+  SidebarTrigger,
+  SidebarMenu,
+} from "@/components/ui/sidebar";
+
+import {
+  AreaHighlight,
+  Highlight,
+  PdfHighlighter,
+  PdfLoader,
+  Popup,
+  Tip,
+} from "react-pdf-highlighter";
+import type {
+  Content,
+  IHighlight,
+  NewHighlight,
+  ScaledPosition,
+} from "react-pdf-highlighter";
+
+import "react-pdf-highlighter/dist/style.css";
+
+const getNextId = () => String(Math.random()).slice(2);
+
+const parseIdFromHash = () =>
+  document.location.hash.slice("#highlight-".length);
+
+const resetHash = () => {
+  document.location.hash = "";
+};
+
+const HighlightPopup = ({
+  comment,
+}: {
+  comment: { title: string; text: string; emoji: string };
+}) =>
+  comment.text ? (
+    <div className="Highlight__popup">
+      {comment.emoji} {comment.title}
+    </div>
+  ) : null;
+
+const PRIMARY_PDF_URL = "https://arxiv.org/pdf/1708.08021";
+const SECONDARY_PDF_URL = "https://arxiv.org/pdf/1604.02480";
+
+function DocumentViewer({ pdfUrl = "https://arxiv.org/pdf/1708.08021" }) {
+  // const searchParams = new URLSearchParams(document.location.search);
+  const initialUrl = PRIMARY_PDF_URL;
+
+  const [url, setUrl] = useState(initialUrl);
+  const [highlights, setHighlights] = useState<Array<IHighlight>>([]);
+
+  const resetHighlights = () => {
+    setHighlights([]);
+  };
+
+  const scrollViewerTo = useRef((highlight: IHighlight) => {});
+
+  const scrollToHighlightFromHash = useCallback(() => {
+    const highlight = getHighlightById(parseIdFromHash());
+    if (highlight) {
+      scrollViewerTo.current(highlight);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("hashchange", scrollToHighlightFromHash, false);
+    return () => {
+      window.removeEventListener(
+        "hashchange",
+        scrollToHighlightFromHash,
+        false
+      );
+    };
+  }, [scrollToHighlightFromHash]);
+
+  const getHighlightById = (id: string) => {
+    return highlights.find((highlight) => highlight.id === id);
+  };
+
+  const addHighlight = (highlight: NewHighlight) => {
+    console.log("Saving highlight", highlight);
+    setHighlights((prevHighlights) => [
+      { ...highlight, id: getNextId() },
+      ...prevHighlights,
+    ]);
+  };
+
+  const updateHighlight = (
+    highlightId: string,
+    position: Partial<ScaledPosition>,
+    content: Partial<Content>
+  ) => {
+    console.log("Updating highlight", highlightId, position, content);
+    setHighlights((prevHighlights) =>
+      prevHighlights.map((h) => {
+        const {
+          id,
+          position: originalPosition,
+          content: originalContent,
+          ...rest
+        } = h;
+        return id === highlightId
+          ? {
+              id,
+              position: { ...originalPosition, ...position },
+              content: { ...originalContent, ...content },
+              ...rest,
+            }
+          : h;
+      })
+    );
+  };
+
+  return (
+    <div className="flex h-full">
+      <div className="h-full">
+        <Sidebar
+          collapsible="icon"
+          className="overflow-hidden [&>[data-sidebar=sidebar]]:flex-row"
+        >
+          <AppSidebar />
+          <Sidebar collapsible="none" className="w-full h-full">
+            <SidebarHeader className="gap-3.5 border-b p-4">
+              <div className="flex w-full items-center justify-between">
+                <div className="text-base font-medium text-foreground">
+                  {/* TODO: TW - Update with documents name */}
+                  Document Name Here...
+                </div>
+              </div>
+              {/* TODO: TW - Can add searchinf of annotations here */}
+              {/* <SidebarInput className="" placeholder="Type to search..." /> */}
+            </SidebarHeader>
+            <SidebarContent className="flex-1">
+              <div className="h-full w-full">
+                <AnnotationBar
+                  highlights={highlights}
+                  resetHighlights={() => setHighlights([])}
+                />
+              </div>
+            </SidebarContent>
+          </Sidebar>
+        </Sidebar>
+      </div>
+
+      <div className="flex flex-1 flex-col">
+        <SidebarInset className="">
+          <div className="flex w-full flex-1 flex-col">
+            <div className="h-full relative overflow-auto">
+              <PdfLoader url={pdfUrl} beforeLoad={<div>Loading PDF...</div>}>
+                {(pdfDocument) => (
+                  <>
+                    <div className="absolute inset-0">
+                      <PdfHighlighter
+                        pdfDocument={pdfDocument}
+                        enableAreaSelection={(event) => event.altKey}
+                        onScrollChange={resetHash}
+                        scrollRef={(scrollTo) => {
+                          scrollViewerTo.current = scrollTo;
+                          scrollToHighlightFromHash();
+                        }}
+                        onSelectionFinished={(
+                          position,
+                          content,
+                          hideTipAndSelection,
+                          transformSelection
+                        ) => {
+                          return (
+                            <div className="flex flex-row gap-2">
+                              <CustomTip
+                                onOpen={transformSelection}
+                                onConfirm={(comment) => {
+                                  addHighlight({
+                                    content,
+                                    position,
+                                    comment: {
+                                      title: comment.title || "No Title",
+                                      text: comment.text || "No comment",
+                                      emoji: "💬",
+                                    },
+                                  });
+                                  hideTipAndSelection();
+                                }}
+                              />
+                              {/* <AiTip
+                                onOpen={transformSelection}
+                                onConfirm={(comment) => {
+                                  addHighlight({
+                                    content,
+                                    position,
+                                    comment: {
+                                      title: comment.title || "No Title",
+                                      text: comment.text || "No comment",
+                                      emoji: "💬",
+                                    },
+                                  });
+                                  hideTipAndSelection();
+                                }}
+                              /> */}
+                            </div>
+                          );
+                        }}
+                        highlightTransform={(
+                          highlight,
+                          index,
+                          setTip,
+                          hideTip,
+                          viewportToScaled,
+                          screenshot,
+                          isScrolledTo
+                        ) => {
+                          const isTextHighlight = !highlight.content?.image;
+                          const component = isTextHighlight ? (
+                            <Highlight
+                              isScrolledTo={isScrolledTo}
+                              position={highlight.position}
+                              comment={highlight.comment}
+                            />
+                          ) : (
+                            <AreaHighlight
+                              isScrolledTo={isScrolledTo}
+                              highlight={highlight}
+                              onChange={(boundingRect) => {
+                                updateHighlight(
+                                  highlight.id,
+                                  {
+                                    boundingRect:
+                                      viewportToScaled(boundingRect),
+                                  },
+                                  { image: screenshot(boundingRect) }
+                                );
+                              }}
+                            />
+                          );
+                          return (
+                            <Popup
+                              popupContent={<HighlightPopup {...highlight} />}
+                              onMouseOver={(popupContent) =>
+                                setTip(highlight, (highlight) => popupContent)
+                              }
+                              onMouseOut={hideTip}
+                              key={index}
+                            >
+                              {component}
+                            </Popup>
+                          );
+                        }}
+                        highlights={highlights}
+                      ></PdfHighlighter>
+                    </div>
+                  </>
+                )}
+              </PdfLoader>
+            </div>
+          </div>
+        </SidebarInset>
+      </div>
+    </div>
+  );
+}
+
+export default DocumentViewer;
---
+++ frontend/components/app-side-bar.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import * as React from "react";
+import { Home, Search, Trash2, File  } from "lucide-react";
+import { GiParrotHead } from "react-icons/gi";
+import { useRouter } from 'next/navigation'
+import { NavUser } from "@/components/NavSidebar";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarTrigger,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+
+// This is sample data
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Home",
+      url: "/",
+      icon: Home,
+      isActive: false,
+    },
+    {
+      title: "Analyze",
+      url: "/analyze",
+      icon: Search,
+      isActive: false,
+    },
+    {
+      title: "Files",
+      url: "#",
+      icon: File,
+      isActive: false,
+    },
+    {
+      title: "Trash",
+      url: "#",
+      icon: Trash2,
+      isActive: false,
+    },
+  ],
+};
+
+function AppSidebar() {
+  const [activeItem, setActiveItem] = React.useState(data.navMain[0]);
+  const { setOpen } = useSidebar();
+  const router = useRouter();
+
+  return (
+    <>
+      {/* This is the first sidebar */}
+      {/* We disable collapsible and adjust width to icon. */}
+      {/* This will make the sidebar appear as icons. */}
+      <Sidebar
+        collapsible="none"
+        className="!w-[calc(var(--sidebar-width-icon)_+_1px)] border-r"
+      >
+        <SidebarHeader className="">
+          <SidebarMenu className="border-b border-sidebar-border pb-4 space-y-2">
+            <SidebarMenuItem className="">
+              <SidebarMenuButton
+                tooltip=""
+                size="lg"
+                asChild
+                className="md:h-8 md:p-0"
+              >
+                <SidebarTrigger
+                  className="flex aspect-square size-8 items-center justify-center bg-sidebar-primary"
+                  onClick={() => console.log("Sidebar triggered")}
+                />
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem className="">
+              <SidebarMenuButton
+                tooltip=""
+                size="md"
+                asChild
+                className="flex aspect-square size-8 items-center justify-center bg-green-500"
+              >
+                <GiParrotHead />
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
+        <SidebarContent className="">
+          <SidebarGroup className="">
+            <SidebarGroupContent className="px-1.5 md:px-0">
+              <SidebarMenu className="">
+                {data.navMain.map((item) => (
+                  <SidebarMenuItem className="" key={item.title}>
+                    <SidebarMenuButton
+                      tooltip={{
+                        children: item.title,
+                        hidden: false,
+                      }}
+                      onClick={() => {
+                        setActiveItem(item);
+                        router.push(item.url);
+                        // TW - Can make certain menu items open the bar if needed
+                        // setOpen(true);
+                      }}
+                      isActive={activeItem?.title === item.title}
+                      className="px-2.5 md:px-2"
+                    >
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter className="">
+          <NavUser user={data.user} />
+        </SidebarFooter>
+      </Sidebar>
+    </>
+  );
+}
+
+export default AppSidebar;
