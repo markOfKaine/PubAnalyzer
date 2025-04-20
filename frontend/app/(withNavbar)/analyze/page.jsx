5f8592369b6e64c2a3ff33c0d8640c096ec3b42a
Tim West <timr200019@yahoo.com>
2025-04-20 00:10:50 -0500

    Added Improved Viewer Page
    C: Viewer now uses a sticky sidebar instead of a header based nav bar
    C: Annotation bar can now be collpased

--- frontend/app/(withNavbar)/analyze/page.jsx
+++ frontend/app/(withNavbar)/analyze/page.jsx
@@ -6,7 +6,7 @@ function Analyze() {
     <div className="max-w-2xl mx-auto space-y-6">
       <h1 className="text-2xl font-bold my-6 text-card-foreground">Analyze PubMed Article</h1>
       <PMCIDForm />
-      <PDFUploadCard />
+      {/* <PDFUploadCard /> */}
     </div>
   );
 }
