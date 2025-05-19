35088a7bec84b74c8a8acb376d28c8c6dba70bcd
Twest19 <timr200019@yahoo.com>
2025-05-19 14:05:31 -0500

    Fixed PDFViewer not displaying document

---
+++ frontend/components/PDFViewer.jsx
@@ -0,0 +1,43 @@
+
+import AppSidebar from "@/components/app-side-bar";
+import DocumentViewer from "@/components/DocumentViewer";
+import PDFSiderBar from "@/components/PDFSiderBar";
+import {
+  SidebarInset,
+  Sidebar,
+  SidebarContent,
+} from "@/components/ui/sidebar";
+import { PDFProvider } from "@/contexts/PDFContext";
+import PDFSiderBarHeader from "@/components/PDFSideBarHeader";
+
+function PDFViewer({ pdfURL }) {
+  return (
+    <PDFProvider initialUrl={pdfURL}>
+      <div className="flex h-full">
+        <div className="h-full">
+          <Sidebar
+            collapsible="icon"
+            className="overflow-hidden [&>[data-sidebar=sidebar]]:flex-row"
+          >
+            <AppSidebar />
+            <Sidebar collapsible="none" className="w-full h-full">
+              <PDFSiderBarHeader className="gap-3.5 border-b p-4" />
+              <SidebarContent className="flex-1">
+                <div className="h-full w-full">
+                  <PDFSiderBar />
+                </div>
+              </SidebarContent>
+            </Sidebar>
+          </Sidebar>
+        </div>
+        <div className="flex flex-1 flex-col">
+          <SidebarInset className="">
+            <DocumentViewer />
+          </SidebarInset>
+        </div>
+      </div>
+    </PDFProvider>
+  );
+}
+
+export default PDFViewer;
