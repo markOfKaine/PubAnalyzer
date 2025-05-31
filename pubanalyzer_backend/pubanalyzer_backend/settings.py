d91bee17dd8d05237f8dfe175beb047c59f24a4c
Bryan Valadez <Bval.business@gmail.com>
2025-05-30 20:26:43 -0500

    REMOVE FOR PROD - Added hosts back for local testing

--- pubanalyzer_backend/pubanalyzer_backend/settings.py
+++ pubanalyzer_backend/pubanalyzer_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["api.pubanalyzer.live"]
+ALLOWED_HOSTS = ["api.pubanalyzer.live", "localhost", "127.0.0.1"]
 
 SESSION_COOKIE_SECURE = False # Ensures the session cookie is only sent over HTTPS
 
